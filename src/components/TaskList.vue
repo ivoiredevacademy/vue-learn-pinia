4bbd8828190a010673a4a34816a3bf6588110381
Juvenal Pengele <juvenal.pengele@gmail.com>
2022-05-21 11:07:11 +0000

    Add Storage class functionnality in order tosave tasks in the localStorage

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="py-6">
-    <h1 class="text-4xl my-10">Taches</h1>
+    <h1 class="text-4xl mt-10 mb-4">Taches</h1>
 
     <div>
       <div class="bg-white rounded-lg flex px-4 py-4 mb-4">
@@ -19,10 +19,33 @@
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quas recusandae sit minima
           </p>
           <div class="flex justify-end mt-4">
-            <button class="w-7 h-7 bg-blue-100 text-blue-700 rounded-lg mr-2">
+            <button class="w-7 h-7 bg-blue-100 text-blue-700 rounded mr-2">
               <i class="las la-pen"></i>
             </button>
-            <button class="w-7 h-7 bg-red-100 text-red-700 rounded-lg">
+            <button class="w-7 h-7 bg-red-100 text-red-700 rounded">
+              <i class="las la-trash"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-white rounded-lg flex px-4 py-4 mb-4">
+        <div class="relative mr-2">
+          <div class="round mr-2">
+            <input type="checkbox" id="task1" />
+            <label for="task1"></label>
+          </div>
+        </div>
+        <div class="pl-4">
+          <span class="text-xl font-semibold">Task #1</span>
+          <p class="text-gray-500 text-sm">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quas recusandae sit minima
+          </p>
+          <div class="flex justify-end mt-4">
+            <button class="w-7 h-7 bg-blue-100 text-blue-700 rounded mr-2">
+              <i class="las la-pen"></i>
+            </button>
+            <button class="w-7 h-7 bg-red-100 text-red-700 rounded">
               <i class="las la-trash"></i>
             </button>
         </div>
@@ -43,10 +66,10 @@
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quas recusandae sit minima
           </p>
           <div class="flex justify-end mt-4">
-            <button class="w-7 h-7 bg-blue-100 text-blue-700 rounded-lg mr-2">
+            <button class="w-7 h-7 bg-blue-100 text-blue-700 rounded mr-2">
               <i class="las la-pen"></i>
             </button>
-            <button class="w-7 h-7 bg-red-100 text-red-700 rounded-lg">
+            <button class="w-7 h-7 bg-red-100 text-red-700 rounded">
               <i class="las la-trash"></i>
             </button>
         </div>
@@ -67,10 +90,10 @@
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quas recusandae sit minima
           </p>
           <div class="flex justify-end mt-4">
-            <button class="w-7 h-7 bg-blue-100 text-blue-700 rounded-lg mr-2">
+            <button class="w-7 h-7 bg-blue-100 text-blue-700 rounded mr-2">
               <i class="las la-pen"></i>
             </button>
-            <button class="w-7 h-7 bg-red-100 text-red-700 rounded-lg">
+            <button class="w-7 h-7 bg-red-100 text-red-700 rounded">
               <i class="las la-trash"></i>
             </button>
         </div>
@@ -78,30 +101,6 @@
  
       </div>
 
-      <div class="bg-white rounded-lg flex px-4 py-4 mb-4">
-        <div class="relative mr-2">
-          <div class="round mr-2">
-            <input type="checkbox" id="task1" />
-            <label for="task1"></label>
-          </div>
-        </div>
-        <div class="pl-4">
-          <span class="text-xl font-semibold">Task #1</span>
-          <p class="text-gray-500 text-sm">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quas recusandae sit minima
-          </p>
-          <div class="flex justify-end mt-4">
-            <button class="w-7 h-7 bg-blue-100 text-blue-700 rounded-lg mr-2">
-              <i class="las la-pen"></i>
-            </button>
-            <button class="w-7 h-7 bg-red-100 text-red-700 rounded-lg">
-              <i class="las la-trash"></i>
-            </button>
-        </div>
-      </div>
- 
-      </div>
-      
     </div>
   </div>
   
--- src/utils/Storage.js
+++ src/utils/Storage.js
@@ -3,20 +3,38 @@ export default new class Storage {
         this.storage = localStorage;
     }
 
+    #save(tasks) {
+        this.setItem('tasks', tasks);
+    }
+
     fetchAll() {
-
+        const serializedTasks = this.storage.getItem('tasks');
+        return JSON.parse(serializedTasks);
     }
 
-    save(data) {
-
+    create(newTask) {
+        const tasks = this.fetchAll();
+        const serializedTasks = JSON.stringify([...tasks, newTask]);
+        this.#save(serializedTasks);
     }
 
-    remove(id) {
-
+    remove(taskId) {
+        const tasks = this.fetchAll();
+        const filteredTasks = tasks.filter((task) => task.id !== taskId);
+        this.#save(filteredTasks);
     }
 
-    update(id, data) {
-        
+    update(updatedTask) {
+        const tasks = this.fetchAll();
+        const updatedTasks = tasks.map((task) => {
+            if(updatedTask.id === task.id) {
+                return updatedTask;
+            }
+
+            return task;
+        })
+
+        this.#save(updatedTasks);
     }
 
 }
